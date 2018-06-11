6cc5f5864b8ff42d4eddeeae0460afbfb268ae7e
Damian Mee <spam@meedamian.com>
2018-06-12 00:10:53 +0700

    init commit

---
+++ invoice.go
@@ -0,0 +1,24 @@
+package main
+
+var allowedColors = []string{
+	"#ffffff",
+	"#e4e4e4",
+	"#888888",
+	"#222222",
+	"#e4b4ca",
+	"#d4361e",
+	"#db993e",
+	"#8e705d",
+	"#e6d84e",
+	"#a3dc67",
+	"#4aba38",
+	"#7fcbd0",
+	"#5880a8",
+	"#3919d1",
+	"#c27ad0",
+	"#742671",
+}
+
+func getInvoice() {
+
+}
